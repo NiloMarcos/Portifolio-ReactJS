c4a2165465e5780b66ea8b5106fefc58af7914f5
Nilo Freitas <nilo.n2m15@gmail.com>
2022-04-23 13:40:23 -0300

    component portifolio fineshed

--- src/components/Portifolio/index.jsx
+++ src/components/Portifolio/index.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
 
+import './portifolio.css';
+
+import Project from '../../assets/project.png';
+
 export default function Portifolio(){
   return(
     <section id="portifolio">
-      <p>Portifolio</p>
+      <h5>My Recent Work</h5>
+      <h2>Portifolio</h2>
+
+      <div className="container portifolio-container">
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+        
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+        
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+        
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+        
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+        
+        <article className="portifolio-item">
+          <div className="portifolio-item-img">
+            <img src={Project} alt="Imagem do projeto" />
+          </div>
+          <h3>This is a portifolio item title</h3>
+          <a href="https://github.com" className="btn">GitHub</a>
+        </article>
+      </div>
     </section>
   );
 }
