84e1f4cc07f4f1ce35eaa7475be82239cab66e3b
Nilo Freitas <nilo.n2m15@gmail.com>
2022-04-23 11:29:57 -0300

    component services fineshed

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,9 +1,110 @@
 import React from 'react';
 
+import './services.css';
+
+import { BsCheck2Circle } from 'react-icons/bs';
+
 export default function Services(){
   return(
     <section id="services">
-      <p>Services</p>
+      <h5>What i Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services-container">
+        <article className="service">
+          <div className="service-head">
+            <h3>Desenvolvimento Lading Pages</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* Fim do que ofereço */}
+
+        <article className="service">
+          <div className="service-head">
+            <h3>Desenvolvimento de sites</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+
+
+        <article className="service">
+          <div className="service-head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service-list">
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+            <li>
+              <BsCheck2Circle className="service-list-icon"/>
+              <p>Loren, ipsum dolor sit amet consectetur elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   );
 }
