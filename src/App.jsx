2be8ceacb826a4a3c8d2b3f9f7a70bce953bed3f
Nilo Freitas <nilo.n2m15@gmail.com>
2022-04-25 12:35:13 -0300

    implement of toastfy

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import Home from './pages/Home';
 
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
+
 export default function App(){
   return(
     <main>
       <Home />
+      <ToastContainer autoClose={3000} />
     </main>
   )
 }
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,6 +8,8 @@ import { BsMessenger } from 'react-icons/bs';
 
 import emailjs from 'emailjs-com';
 
+import {toast} from 'react-toastify';
+
 export default function Contact(){
   const form = useRef();
 
@@ -17,6 +19,8 @@ export default function Contact(){
     emailjs.sendForm('service_c0doxtr', 'template_1hjkj5o', form.current, 'd-9mV4jdtRyOu6gWk');
 
     e.target.reset()
+
+    toast.success('Formulario enviado com sucesso');
   }
 
 
